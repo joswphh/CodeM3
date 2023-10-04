4d5d9802b6d92fef2ee408f82039e6e52e8c7d52
Joseph <Medranojoseph28@gmail.com>
2023-10-04 09:20:00 -0500

    WarmUps

---
+++ WarmUps/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String personName = "Joseph Medrano";
+        int personAge = 22;
+        double productPrice = 19.99;
+        float tempatureInCelsius = 27.5f;
+        boolean isLoggedIn = false;
+    }
+}
